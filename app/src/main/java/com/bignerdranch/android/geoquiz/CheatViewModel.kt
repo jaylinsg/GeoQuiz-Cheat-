3c4011deb23f96fed2b158359b3ca634a9e738c9
JaStaGa <jaylinstarlinganaway@gmail.com>
2023-10-02 14:57:47 -0400

    reset

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatViewModel.kt
@@ -1,6 +1,6 @@
 package com.bignerdranch.android.geoquiz
 import androidx.lifecycle.ViewModel
-
+// created to fix loop issue
 class CheatViewModel: ViewModel() {
 
     var answerWasClicked = false
